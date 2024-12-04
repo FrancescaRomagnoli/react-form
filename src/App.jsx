0266337ee3a3e2716d9d6d8ff533a3e4907802f8
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-12-04 15:48:24 +0100

    add input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
+  // const articlesList = [];
+  const [newArticleTitle, setNewArticleTitle] = useState("");
+
+  const handlePostSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleNewTitle = (e) => {
+    setNewArticleTitle(e.target.value);
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <header>
+        <h1>Crea un nuovo post</h1>
+      </header>
+      <main>
+        <div>
+          <form onSubmit={handlePostSubmit}>
+            <label htmlFor="post-title">Inserisci il titolo del post</label>
+            <input
+              onChange={handleNewTitle}
+              value={newArticleTitle}
+              type="text"
+              id="post-title"
+            />
+            <button>Crea</button>
+          </form>
+        </div>
+        <div>
+          <h3>I tuoi post:</h3>
+          <h4>Titolo del nuovo post</h4>
+        </div>
+      </main>
     </>
   );
 }
